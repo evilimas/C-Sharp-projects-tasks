7fb7a60c0efaf9964c5b4c66d3bb7977d31b64a4
Egidijus Vilimas <eg.vilimas@gmail.com>
2024-11-13 14:19:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("Hello and welcome to a guess a number game!");
+
+var random = new Random();
+var number = random.Next(1, 100);
+var guessCount = 0;
+
+while (true)
+{
+    Console.Write("Guess a number ");
+    var guess = int.Parse(Console.ReadLine());
+    guessCount++;
+
+    if (guess < number)
+    {
+        Console.WriteLine("You guessed to low, try again!");
+    }
+    else if (guess > number)
+    {
+        Console.WriteLine("You guessed to high, try again!");
+    }
+    else
+    {
+        Console.WriteLine($"You guessed correct ,you managed to guess in {guessCount} times");
+        break;
+    }
+}
+   
